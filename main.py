d135764da7a2ac0ba732a6ec874de5a7cd84ed6c
Geoff Hawes <60487989+ghawes85@users.noreply.github.com>
2022-08-31 06:01:45 -0600

    fixed link

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ logger.addHandler(logger_file_handler)
 if __name__ == "__main__":
     
 
-    r = requests.get('http://localhost:5005/automation/renewal/DwDpmP7ClwXz3B6b4uQb')
-    #print(r.text)
-    #r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
+    #r = requests.get('http://localhost:5005/automation/renewal/DwDpmP7ClwXz3B6b4uQb')
+    r = requests.get('https://crdhaportal-beta.azurewebsites.net/automation/renewal/DwDpmP7ClwXz3B6b4uQb')
+    #r = requests.get('https://crdhaportal.azurewebsites.net/automation/renewal/DwDpmP7ClwXz3B6b4uQb')
     if r.status_code == 200:
         r.encoding='utf-8-sig'
         data = json.loads(r.text)
